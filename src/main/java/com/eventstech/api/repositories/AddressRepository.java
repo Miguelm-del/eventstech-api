bac606701a05e0655020351d867cbdab3174d0d0
Miguel Márcio <miguelmarcio.dev@gmail.com>
2025-01-29 19:29:26 -0300

    fix: extends in AddressRepository

--- src/main/java/com/eventstech/api/repositories/AddressRepository.java
+++ src/main/java/com/eventstech/api/repositories/AddressRepository.java
@@ -1,8 +1,9 @@
 package com.eventstech.api.repositories;
 
+import com.eventstech.api.domain.address.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface AddressRepository extends JpaRepository<AddressRepository, UUID> {
+public interface AddressRepository extends JpaRepository<Address, UUID> {
 }
